3cd86c7c4d5d28936958de370a661e9a70129da6
sumit kumar <sumitmathur170@gmail.com>
2023-07-23 16:36:06 +0530

    graph questions

---
+++ graph/18 Count Strongly Connected Components (Kosaraju’s Algorithm).cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs(int node, unordered_map<int,bool> &vis,stack<int> &st,
+ unordered_map<int, list<int>> &adj){
+
+
+	vis[node] = true;
+
+	for(auto nbr : adj[node]){
+		if(!vis[nbr]){
+			dfs(nbr,vis,st,adj);
+		}
+	}
+
+	// topo
+	st.push(node);
+ }
+
+void revdfs(int node, unordered_map<int,bool> &vis, unordered_map<int, list<int>> &transpose){
+
+	 vis[node] = true;
+
+	 for(auto nbr : transpose[node]){
+		 if(!vis[nbr]){
+			revdfs(nbr,vis,transpose);
+		 }
+	 }
+ }
+
+
+int stronglyConnectedComponents(int v, vector<vector<int>> &edges)
+{
+	// Write your code here.
+
+	// adj list
+
+	unordered_map<int, list<int>> adj;
+
+	for(int i=0;i<edges.size();i++){
+		int u = edges[i][0];
+		int v = edges[i][1];
+
+		adj[u].push_back(v);
+	}
+
+	// topo 
+
+	stack<int> st;
+
+	unordered_map<int,bool> vis;
+
+	for(int i=0;i<v;i++){
+		if(!vis[i]){
+			dfs(i,vis,st,adj);
+		}
+	}
+
+	// create transpose graph
+
+	unordered_map<int, list<int>> transpose;
+
+	for(int i=0;i<v;i++){
+		vis[i] = 0;
+		for(auto nbr:adj[i]){
+			transpose[nbr].push_back(i);
+		}
+	}
+
+
+	// dfs coll using above ordering
+	int count = 0;
+	while(!st.empty()){
+		int top = st.top();
+		st.pop();
+
+		if(!vis[top]){
+			count++;
+			revdfs(top,vis,transpose);
+		}
+	}
+
+	return count;
+}
